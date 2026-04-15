1490a0ee8763a566252196c8cc7e4293608d4e21
Javohir <qobiljonsharipov33@gmail.com>
2026-04-15 11:10:41 +0500

    Commit yana

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,4 @@ python manage.py migrate --no-input
 
 python manage.py createadmin
 
-# WSGI (gunicorn) cannot serve WebSockets. Use ASGI so ESP32 + browser can connect.
-daphne -b 0.0.0.0 -p 8000 core.asgi:application
+gunicorn core.asgi:application -k uvicorn.workers.UvicornWorker
